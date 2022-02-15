624827d7523ba80891685be8188fea7b77db3c10
dlz917 <joanschmitvalles@yahoo.es>
2022-02-15 16:54:38 +0100

    questions

--- RoboRally/src/tableau/CaseTableau.java
+++ RoboRally/src/tableau/CaseTableau.java
@@ -1,9 +1,10 @@
 package tableau;
 import tableau.Lignes;;
-public class CaseTableau {
+public abstract class CaseTableau {
 
 	private final Lignes ligne=Lignes.A;
 	private final int colonne=1;
 	private boolean occupe=false;
+	private boolean laser=false;
 	private EtatCase etat;
 }
