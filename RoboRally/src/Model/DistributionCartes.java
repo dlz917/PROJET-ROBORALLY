939f638c236f28044a2a9e00d1bca7f6cd604620
barna <barna@192.168.224.107>
2022-02-18 09:02:46 +0100

    modifs

--- RoboRally/src/Model/DistributionCartes.java
+++ RoboRally/src/Model/DistributionCartes.java
@@ -1,4 +1,4 @@
-package Projet;
+package Model;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Random;
--- RoboRally/src/Model/StockCartes.java
+++ RoboRally/src/Model/StockCartes.java
@@ -1,4 +1,4 @@
-package Projet;
+package Model;
 
 
 import java.util.ArrayList;
